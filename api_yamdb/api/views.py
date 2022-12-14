1521d5a1394720ff80fa399b2c5cacb06d358916
chakchakk <ioann.chimrov@gmail.com>
2022-12-14 15:48:41 +0300

    create category viewset

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,10 @@
+from api.serializers import CategorySerializer
+from reviews.models import Category
+from api.viewsets import CreateListDestroyViewSet
+
+
+class CategoryViewSet(CreateListDestroyViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    # FIXME add "permission_classes = (IsAdmin,)" after they are written 
+    lookup_field = 'slug'
