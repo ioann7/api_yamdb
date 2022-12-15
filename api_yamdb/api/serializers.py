d27abf6dfea4d997600e0b4c759101da30f59f01
chakchakk <ioann.chimrov@gmail.com>
2022-12-15 14:52:27 +0300

    set unique slug for category serializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from reviews.models import Category
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=256)
+    slug = serializers.SlugField(
+        max_length=50,
+        validators=(UniqueValidator(queryset=Category.objects.all()),)
+    )
 
     class Meta:
         fields = ('id', 'name', 'slug')
