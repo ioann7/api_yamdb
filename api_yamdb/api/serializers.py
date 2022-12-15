78409830c26555244fdb5c05f3957fd2ddb056eb
chakchakk <ioann.chimrov@gmail.com>
2022-12-15 15:38:11 +0300

    create serializers and viewsets for genre and title

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
-from reviews.models import Category
+from reviews.models import Category, Genre, Title
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -8,3 +9,34 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'name', 'slug')
         model = Category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=256)
+    slug = serializers.SlugField(
+        max_length=50,
+        validators=(UniqueValidator(queryset=Genre.objects.all()),)
+    )
+
+    class Meta:
+        fields = ('name', 'slug')
+        model = Genre
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=256)
+    description = serializers.CharField(required=False)
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all()
+    )
+    genre = serializers.SlugRelatedField(
+        many=True,
+        slug_field='slug',
+        source='genres',
+        queryset=Genre.objects.all()
+    )
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+        model = Title
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework import filters
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.viewsets import ModelViewSet
 
-from api.serializers import CategorySerializer
-from reviews.models import Category
+from api.serializers import (CategorySerializer,
+                             GenreSerializer, TitleSerializer)
+from reviews.models import Category, Genre, Title
 from api.viewsets import CreateListDestroyViewSet
 
 
@@ -14,3 +16,18 @@ class CategoryViewSet(CreateListDestroyViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     pagination_class = LimitOffsetPagination
+
+
+class GenreViewSet(CreateListDestroyViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    # FIXME add "permission_classes = (IsAdminOrReadOnly,)" after they are written 
+    lookup_field = 'slug'
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+
+class TitleViewSet(ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    # FIXME add "permission_classes = (IsAdminOrReadOnly,)" after they are written 
